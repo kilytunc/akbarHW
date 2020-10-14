4963001538f043e7e95ed1fe3d773d83e3a7e713
MehmetAkifTUNC <kilyneu@gmail.com>
2020-10-14 20:56:40 +0200

    initial Commit First 4 step of HW

---
+++ src/test/java/helloAKBAR_HW.java
@@ -0,0 +1,8 @@
+public class helloAKBAR_HW {
+    public static void main(String[] args) {
+        System.out.println("create brandnew project ++ ");
+        System.out.println("Enable    V C S   ++ ");
+        System.out.println("Ignore the files that you need to ignore ++");
+
+    }
+}
